e75c831e344dd52f7498f643461202d561ec47af
Romi Qelibari <bajram.qelibari@googlemail.com>
2021-03-26 22:30:14 +0100

    .

--- Tic Tac Toe/gamestate/gamefield.py
+++ Tic Tac Toe/gamestate/gamefield.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import ceil
 from . import SQUARE_GAMEFIELD_SIZE
 
 
@@ -9,51 +10,58 @@ class Column_Out_Of_Field_Exception(Exception):
 class Row_Out_Of_Field_Exception(Exception):
     pass
 
+
+@dataclass
 class Coordinate:
+    column: int
+    row: int
+
     def __init__(self, column: int, row: int):
         self.column = column
         self.row = row
         self._size = SQUARE_GAMEFIELD_SIZE
-        self._validate_coordinates_or_throw_exception()
+        self._validate_coordinates_or_throw_exception( )
 
-    def _validate_coordinates_or_throw_exception(self, column: int, row: int) -> bool:
-        is_column_in_gamefield = 1 <= column <= self._size
-        is_row_in_gamefield = 1 <= row <= self._size
+    def _validate_coordinates_or_throw_exception(self) -> bool:
+        is_column_in_gamefield = 1 <= self.column <= self._size
+        is_row_in_gamefield = 1 <= self.row <= self._size
 
         if not is_column_in_gamefield:
-            raise Column_Out_Of_Field_Exception("Invalid row (x coordinate) given (out of gamefield).")
+            raise Column_Out_Of_Field_Exception(f"Invalid column ({self.column}) given (out of gamefield).")
 
         if not is_row_in_gamefield:
-            raise Row_Out_Of_Field_Exception("Invalid row (y coordinate) given (out of gamefield).")
+            raise Row_Out_Of_Field_Exception(f"Invalid row ({self.row}) given (out of gamefield).")
 
     def as_index(self) -> int:
         horizontal_field_index = self.column
-        vertical_push = (self.row - 1) * self.size
+        vertical_push = (self.row - 1) * self._size
 
         return horizontal_field_index + vertical_push
 
     @staticmethod
     def from_index(index) -> 'Coordinate':
-        column = index % SQUARE_GAMEFIELD_SIZE
-        row = index // SQUARE_GAMEFIELD_SIZE + 1
+        row = ceil(index / SQUARE_GAMEFIELD_SIZE)
+        column = index - ((row - 1) * SQUARE_GAMEFIELD_SIZE)
         return Coordinate(column, row)
+
 
 @dataclass
 class Field:
     coordinates: Coordinate
     value: str = ""
 
+
 class Gamefield:
     def __init__(self):
-        self.gamefield = [Field(Coordinate.from_index(idx)) for idx in range(1, self.size ** 2 + 1)]
         self.size = SQUARE_GAMEFIELD_SIZE
+        self.gamefield = [Field(Coordinate.from_index(idx)) for idx in range(1, self.size ** 2 + 1)]
 
     def place_symbol(self, field_coordinates: Coordinate, symbol: str):
         gamefield_index = self._calculate_list_index_from_coordinates(field_coordinates)
         self.gamefield[gamefield_index] = symbol
 
     def _calculate_list_index_from_coordinates(self, field_coordinates: Coordinate) -> int:
-        return field_coordinates.as_index() - 1
+        return field_coordinates.as_index( ) - 1
 
     def _get_field_from_coordinates(self, column, row):
         field_coordinate = Coordinate(column, row)
@@ -78,9 +86,9 @@ class Gamefield:
             yield self._get_field_from_coordinates(from_right_x_coordinate, column)
 
     def get_fields_per_row(self):
-        for column in range(1, self.size + 1):
-            return list(self.get_column_field_values(column))
+        for row in range(1, self.size + 1):
+            yield self.get_row_fields(row)
 
     def get_fields_per_column(self):
-        for row in range(1, self.size + 1):
-            return list(self.get_row_field_values(row))
+        for column in range(1, self.size + 1):
+            yield self.get_column_fields(column)
---
+++ Tic Tac Toe/gamestate/test/gamefield.py
@@ -0,0 +1,69 @@
+import unittest
+from ..gamefield import Coordinate, Gamefield, Field
+from .. import SQUARE_GAMEFIELD_SIZE
+
+
+class Test_Gamefield(unittest.TestCase):
+    def setUp(self) -> None:
+        self.gamefield = Gamefield( )
+
+    def test__calculateListIndexFromCoordinates_firstFieldCoordinates_returns0(self) -> None:
+        field_coordinate = Coordinate(1, 1)
+        list_index = self.gamefield._calculate_list_index_from_coordinates(field_coordinate)
+        self.assertEqual(list_index, 0)
+
+    def test__calculateListIndexFromCoordinates_lastFieldCoordinates_returnsLastIndex(self) -> None:
+        field_coordinate = Coordinate(SQUARE_GAMEFIELD_SIZE, SQUARE_GAMEFIELD_SIZE)
+        list_index = self.gamefield._calculate_list_index_from_coordinates(field_coordinate)
+        self.assertEqual(list_index, SQUARE_GAMEFIELD_SIZE ** 2 - 1)
+
+    def test__getFieldFromCoordinates_firstFieldCoordinates_returnsFirstField(self):
+        field = self.gamefield._get_field_from_coordinates(1, 1)
+        self.assertEqual(field.coordinates.column, 1)
+        self.assertEqual(field.coordinates.row, 1)
+
+    def test__getFieldFromCoordinates_lastFieldCoordinates_returnsLastField(self):
+        field = self.gamefield._get_field_from_coordinates(SQUARE_GAMEFIELD_SIZE, SQUARE_GAMEFIELD_SIZE)
+        self.assertEqual(field.coordinates.column, SQUARE_GAMEFIELD_SIZE)
+        self.assertEqual(field.coordinates.row, SQUARE_GAMEFIELD_SIZE)
+
+    def test_getColumnFields_firstColumn_yieldsFirstColumn(self):
+        generator = self.gamefield.get_column_fields(1)
+        self.assertEqual(next(generator), Field(Coordinate(1, 1), ""))
+        for field in generator:
+            pass
+        self.assertEqual(field, Field(Coordinate(1, SQUARE_GAMEFIELD_SIZE), ""))
+
+    def test_getRowFields_firstRow_yieldsFirstRow(self):
+        generator = self.gamefield.get_row_fields(1)
+        self.assertEqual(next(generator), Field(Coordinate(1, 1), ""))
+        for field in generator: pass
+        self.assertEqual(field, Field(Coordinate(SQUARE_GAMEFIELD_SIZE, 1), ""))
+
+    def test_getLeftDiagonalFields_leftDiagonal_yieldsLeftDiagonalFields(self):
+        generator = self.gamefield.get_left_diagonal_fields( )
+        self.assertEqual(next(generator), Field(Coordinate(1, 1), ""))
+        for field in generator: pass
+        self.assertEqual(field, Field(Coordinate(SQUARE_GAMEFIELD_SIZE, SQUARE_GAMEFIELD_SIZE), ""))
+
+    def test_getRightDiagonalFields_rightDiagonal_yieldsRightDiagonalFields(self):
+        generator = self.gamefield.get_right_diagonal_fields( )
+        self.assertEqual(next(generator), Field(Coordinate(SQUARE_GAMEFIELD_SIZE, 1), ""))
+        for field in generator: pass
+        self.assertEqual(field, Field(Coordinate(1, SQUARE_GAMEFIELD_SIZE), ""))
+
+    def test_getFieldsPerRow_firstRow_returnsFirstRowFieldsAsList(self):
+        rowList = self.gamefield.get_fields_per_row( )
+        self.assertEqual(rowList[0], Field(Coordinate(1, 1), ""))
+        self.assertEqual(rowList[-1], Field(Coordinate(SQUARE_GAMEFIELD_SIZE, 1), ""))
+
+    def test_getFieldsPerRow_lastRow_returnsLastRowFieldsAsList(self):
+        row_list = self.gamefield.get_fields_per_row( )
+        self.assertEqual(row_list[0], Field(Coordinate(1, SQUARE_GAMEFIELD_SIZE), ""))
+        self.assertEqual(row_list[-1], Field(Coordinate(SQUARE_GAMEFIELD_SIZE, SQUARE_GAMEFIELD_SIZE), ""))
+
+    def test_get_fields_per_column(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
